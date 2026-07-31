7f061454971c65349285138fe81127a9e0e3e32b
raccoon <raccoonandnin@mail.ru>
2026-08-01 00:20:52 +0300

    add fixtures

--- tests/UtilsTest.php
+++ tests/UtilsTest.php
@@ -7,9 +7,24 @@ use function Hexlet\Phpunit\Utils\reverseString;
 
 class UtilsTest extends TestCase
 {
+    public function getFixtureFullPath(string $fixtureName): string
+    {
+        $parts = [__DIR__, 'fixtures', $fixtureName];
+        return realpath(implode('/', $parts));
+    }
+
     public function testReverse(): void
     {
         $this->assertEquals('', reverseString(''));
         $this->assertEquals('olleh', reverseString('hello'));
+    }
+
+    public function testReverseWithFixtures(): void
+    {
+        $pathToText = $this->getFixtureFullPath('text.txt');
+        $text = file_get_contents($pathToText);
+        $pathToReversedText = $this->getFixtureFullPath('reversed_text.txt');
+        $reversedText = file_get_contents($pathToReversedText);
+        $this->assertEquals($reversedText, reverseString($text));
     }
 }
